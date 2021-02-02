d042abc77783bb3a408f2b99a397edeb3be72fe8
Arturo Quintana <arturocu81@gmail.com>
2021-02-02 17:14:40 -0500

    - Creation of new methods to support the testing process - Optimized the dependencies between methods

--- tests/helpers.py
+++ tests/helpers.py
@@ -44,12 +44,7 @@ def get_topology_mock():
     return topology
 
 
-def get_topology_with_metadata_mock():
-    """Create a topology with metadata."""
-    switches = {}
-    interfaces = {}
-    links = {}
-    i = 0
+def topology_setting():
 
     switches_to_interface_counts = {"S1": 2, "S2": 2, "S3": 6, "S4": 2,
                                     "S5": 6, "S6": 5, "S7": 2, "S8": 8,
@@ -102,6 +97,18 @@ def get_topology_with_metadata_mock():
         {"reliability": 3, "bandwidth": 100, "delay": 10, "ownership": "A"},
         {"reliability": 3, "bandwidth": 100, "delay": 6},
         {"reliability": 5, "bandwidth": 10, "delay": 105}]
+
+    return links_to_interfaces, links_to_metadata, switches_to_interface_counts
+
+
+def get_topology_with_metadata_mock():
+    """Create a topology with metadata."""
+    switches = {}
+    interfaces = {}
+    links = {}
+    i = 0
+
+    links_to_interfaces, links_to_metadata, switches_to_interface_counts = topology_setting()
 
     for switch in switches_to_interface_counts:
         switches[switch] = get_switch_mock(switch)
@@ -124,64 +131,14 @@ def get_topology_with_metadata_mock():
     topology.switches = switches
     return topology
 
+
 def get_topology_with_metadata():
     """Create a topology with metadata."""
     switches = {}
     interfaces = {}
     links = {}
-    i = 0
 
-    switches_to_interface_counts = {"S1": 2, "S2": 2, "S3": 6, "S4": 2,
-                                    "S5": 6, "S6": 5, "S7": 2, "S8": 8,
-                                    "S9": 4, "S10": 3, "S11": 3,
-                                    "User1": 4, "User2": 2,
-                                    "User3": 2, "User4": 3}
-
-    links_to_interfaces = [["S1:1", "S2:1"], ["S1:2", "User1:1"],
-                           ["S2:2", "User4:1"], ["S3:1", "S5:1"],
-                           ["S3:2", "S7:1"], ["S3:3", "S8:1"],
-                           ["S3:4", "S11:1"],
-                           ["S3:5", "User3:1"], ["S3:6", "User4:2"],
-                           ["S4:1", "S5:2"], ["S4:2", "User1:2"],
-                           ["S5:3", "S6:1"],
-                           ["S5:4", "S6:2"], ["S5:5", "S8:2"],
-                           ["S5:6", "User1:3"], ["S6:3", "S9:1"],
-                           ["S6:4", "S9:2"], ["S6:5", "S10:1"],
-                           ["S7:2", "S8:3"],
-                           ["S8:4", "S9:3"], ["S8:5", "S9:4"],
-                           ["S8:6", "S10:2"],
-                           ["S8:7", "S11:2"], ["S8:8", "User3:2"],
-                           ["S10:3", "User2:1"], ["S11:3", "User2:2"],
-                           ["User1:4", "User4:3"]]
-
-    links_to_metadata = [
-        {"reliability": 5, "bandwidth": 100, "delay": 105},
-        {"reliability": 5, "bandwidth": 100, "delay": 1},
-        {"reliability": 5, "bandwidth": 100, "delay": 10},
-        {"reliability": 5, "bandwidth": 10, "delay": 112},
-        {"reliability": 5, "bandwidth": 100, "delay": 1},
-        {"reliability": 5, "bandwidth": 100, "delay": 1},
-        {"reliability": 3, "bandwidth": 100, "delay": 6},
-        {"reliability": 5, "bandwidth": 100, "delay": 1},
-        {"reliability": 5, "bandwidth": 100, "delay": 10},
-        {"reliability": 1, "bandwidth": 100, "delay": 30, "ownership": "A"},
-        {"reliability": 3, "bandwidth": 100, "delay": 110, "ownership": "A"},
-        {"reliability": 1, "bandwidth": 100, "delay": 40},
-        {"reliability": 3, "bandwidth": 100, "delay": 40, "ownership": "A"},
-        {"reliability": 5, "bandwidth": 100, "delay": 112},
-        {"reliability": 3, "bandwidth": 100, "delay": 60},
-        {"reliability": 3, "bandwidth": 100, "delay": 60},
-        {"reliability": 5, "bandwidth": 100, "delay": 62},
-        {"bandwidth": 100, "delay": 108, "ownership": "A"},
-        {"reliability": 5, "bandwidth": 100, "delay": 1},
-        {"reliability": 3, "bandwidth": 100, "delay": 32},
-        {"reliability": 3, "bandwidth": 100, "delay": 110},
-        {"reliability": 5, "bandwidth": 100, "ownership": "A"},
-        {"reliability": 3, "bandwidth": 100, "delay": 7},
-        {"reliability": 5, "bandwidth": 100, "delay": 1},
-        {"reliability": 3, "bandwidth": 100, "delay": 10, "ownership": "A"},
-        {"reliability": 3, "bandwidth": 100, "delay": 6},
-        {"reliability": 5, "bandwidth": 10, "delay": 105}]
+    links_to_interfaces, links_to_metadata, switches_to_interface_counts = topology_setting()
 
     for switch in switches_to_interface_counts:
         switches[switch] = Switch(switch)
@@ -191,13 +148,9 @@ def get_topology_with_metadata():
         for i in range(1, value + 1):
             str1 = "{}:{}".format(switches[key].dpid, i)
             interface = Interface(str1, i, switches[key])
-            # switches[key].interfaces[str1] = interface
             switches[key].update_interface(interface)
             interfaces[interface.id] = interface
 
-        # for interface in _get_interfaces(value, switches[key]):
-        #     switches[key].interfaces[interface.id] = interface
-        #     interfaces[interface.id] = interface
     i = 0
     for interfaces_str in links_to_interfaces:
         interface_a = interfaces[interfaces_str[0]]
@@ -226,7 +179,6 @@ def get_filter_links_fake(links, metadata=True, **metrics):
     if not metadata:
         return filtered_links_without_metadata
     return filtered_links
-
 # pylint: enable=unused-argument
 
 
