751bf36525645fb9f4f066d82cd796caae69335b
Arturo Quintana <arturocu81@gmail.com>
2021-02-09 09:25:50 -0500

    Refactoring to reduce duplicate code

--- tests/integration/test_exactdelay_results.py
+++ tests/integration/test_exactdelay_results.py
@@ -13,6 +13,13 @@ class TestExactDelayResults(TestCase):
         self.pathfinder = ExactDelayPathfinder()
         self.G = nx.Graph()
 
+    def add_edges(self):
+        edges = [('User1', 'S2', {'delay': 10}), ('User1', 'S3', {'delay': 37}),
+                 ('S2', 'S4', {'delay': 24}), ('S3', 'S4', {'delay': 48}),
+                 ('S3', 'S6', {'delay': 96}), ('S4', 'S5', {'delay': 1}),
+                 ('S6', 'User2', {'delay': 84}), ('S5', 'User2', {'delay': 29})]
+        self.G.add_edges_from(edges)
+
     def test_edpf_can_show_best_path_first(self):
         """Test search function to see if the first result is the best one."""
 
@@ -21,15 +28,10 @@ class TestExactDelayResults(TestCase):
         # of the graph (topology) will affect the outcome of the test which may result in a failure.
         nodes = ['User1', 'S2', 'S3', 'S4', 'S5', 'S6', 'User2']
         self.G.add_nodes_from(nodes)
-        edges = [('User1', 'S2', {'delay': 10}), ('User1', 'S3', {'delay': 37}),
-                 ('S2', 'S4', {'delay': 24}), ('S3', 'S4', {'delay': 48}),
-                 ('S3', 'S6', {'delay': 96}), ('S4', 'S5', {'delay': 1}),
-                 ('S6', 'User2', {'delay': 84}), ('S5', 'User2', {'delay': 29})]
-
-        self.G.add_edges_from(edges)
+        self.add_edges()
 
         # Create result variables and run the test search
-        result = []
+        # result = []
         result = self.pathfinder.search(self.G, 64, 'User1', 'User2')
         first_result = result[0]
         actual = first_result.get('total_delay')  # extract first result
@@ -39,19 +41,13 @@ class TestExactDelayResults(TestCase):
 
     def test_edpf_can_show_empty_result(self):
         """Test search function to see if it can return an empty result."""
-
-        edges = [('User1', 'S2', {'delay': 10}), ('User1', 'S3', {'delay': 37}),
-                 ('S2', 'S4', {'delay': 24}), ('S3', 'S4', {'delay': 48}),
-                 ('S3', 'S6', {'delay': 96}), ('S4', 'S5', {'delay': 1}),
-                 ('S6', 'User2', {'delay': 84}), ('S5', 'User2', {'delay': 29})]
-
-        self.G.add_edges_from(edges)
+        self.add_edges()
 
         # Create isolated node. This node is not connected to another node.
         isolated_nodes = ['S7']
         self.G.add_nodes_from(isolated_nodes)
 
-        result = []
+        # result = []
 
         # Find path to the unreachable node - impossible
         result = self.pathfinder.search(self.G, 64, 'User1', 'S7')
